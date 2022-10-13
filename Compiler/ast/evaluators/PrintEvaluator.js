bcbf898a12d88b047938dc6fef80c0bee12162f1
Tom Mo <motao312@students.cs.ubc.ca>
2022-10-12 20:26:54 -0700

    Evaluator

--- Compiler/ast/evaluators/PrintEvaluator.js
+++
@@ -1,7 +0,0 @@
-import JetejVisitor from "../JetejVisitor";
-
-export default class PrintEvaluator extends JetejVisitor {
-    visitProgram(context, programNode) {
-      console.log(`Encountered a program called ${0}`, programNode.name);
-    };
-}
